19a2d50b1144bb253d6521c48691b774f3d3a19e
Dipesh Aggarwal <dragoonhp@dragoonhp.com>
2017-05-16 19:04:16 +0530

    Added basic chart
    * Using d3, we are showing a very bare bones chart of how much #WannaCry
    devs have earned every day.
    * Also getting exchange rate now,

--- revenue.js
+++ revenue.js
@@ -1,14 +1,16 @@
 var address = ["115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn", "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw", "13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94"];
 var baseAddress = "https://btc.blockr.io/api/v1/address/";
+var exchangeAddress = "https://bitpay.com/api/rates";
 var parsedData = {};
+var exchangeRate = {};
 
-function getJson(tag, address, cb) {
+function getJson(base, tag, address, cb) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             var data = JSON.parse(xhttp.responseText);
 
-            if ( data.status != "success" ) {
+            if ( status in data && data.status != "success" ) {
                 console.log("error");
                 return;
             }
@@ -16,26 +18,27 @@ function getJson(tag, address, cb) {
         }
     }
 
-    xhttp.open("GET", baseAddress + tag + address.join(","));
+    xhttp.open("GET", base + tag + address.join(","));
     xhttp.send();
-}
-
-function getTransactions() {
-    var tag = "txs/";
-    var add = address;
-
-    getJson(tag, add, parseTransactions);
 }
 
 function getBalance() {
     var tag = "balance/";
     var add = address;
 
-    getJson(tag, add, parseBalance);
-    getTransactions();
+    getJson(baseAddress, tag, add, parseBalance);
+    getRate();
+}
+
+function getRate() {
+    var tag = "";
+    var add = []
+
+    getJson(exchangeAddress, tag, add, parseExchange);
 }
 
 function parseTransactions(jsonDump) {
+    var chartData = [];
 
     for ( var i = 0; i < address.length; i++ ) {
         var addressObj = jsonDump.data[i].txs;
@@ -48,7 +51,7 @@ function parseTransactions(jsonDump) {
                 parsedData[time[0]] = [];
                 parsedData[time[0]].push( {
                     totalAmount: 0
-                })
+                });
             }
 
             // Save Data
@@ -60,6 +63,22 @@ function parseTransactions(jsonDump) {
             parsedData[time[0]][0].totalAmount += addressObj[j].amount;
         }
     }
+
+    var keyList = Object.keys(parsedData);
+
+    for ( var k = 0; k < keyList.length; k++ ) {
+        var key = keyList[k]
+
+        if (key === "_meta") {
+            continue;
+        }
+
+        chartData.push({
+            date: key,
+            amount: parsedData[key][0].totalAmount
+        });
+    }
+    return chartData;
 }
 
 function parseBalance(jsonDump) {
@@ -68,12 +87,21 @@ function parseBalance(jsonDump) {
     }
 
     for ( var i = 0; i < address.length; i++ ) {
-        parsedData[ jsonDump.data[i].address ] = jsonDump.data[i].balance;
+        parsedData._meta[ jsonDump.data[i].address ] = jsonDump.data[i].balance;
         parsedData._meta.totalBTC += jsonDump.data[i].balance;
-        console.log(jsonDump.data[i].balance, parsedData._meta.totalBTC)
     }
 
     updateDOM("btcAmount", parsedData._meta.totalBTC);
+}
+
+function parseExchange(jsonDump) {
+    for ( var i = 0; i < jsonDump.length; i++ ) {
+        exchangeRate[ jsonDump[i].code ] = {
+            name: jsonDump[i].name,
+            rate: jsonDump[i].rate
+        }
+    }
+    console.log(exchangeRate);
 }
 
 function updateDOM(id, value) {
